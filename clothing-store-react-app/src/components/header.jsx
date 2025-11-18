8d8fae4e1a29b4ce7a982be7c0438f2a27c0e042
Jauniew21 <jauniew21@gmail.com>
2025-11-17 21:26:44 -0700

    7. header

---
+++ clothing-store-react-app/src/components/header.jsx
@@ -0,0 +1,14 @@
+
+
+const Header = (props) => {
+    return(<div>
+        <p>Logo</p>
+        <p>Home</p>
+        <p>Women</p>
+        <p>Men</p>
+        <p>Browse</p>
+        <p>About</p>
+    </div>)
+}
+
+export default Header;
