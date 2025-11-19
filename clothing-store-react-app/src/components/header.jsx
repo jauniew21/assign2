e9089d11bedff9997ccc46321f821ee9b3d3ddbe
Jauniew21 <jauniew21@gmail.com>
2025-11-18 22:38:37 -0700

    change

--- clothing-store-react-app/src/components/header.jsx
+++ clothing-store-react-app/src/components/header.jsx
@@ -4,7 +4,7 @@ const Header = (props) => {
     return(<div className="flex relative bg-gray-800/50 after:pointer-events-none after:absolute after:inset-x-0 after:bottom-0 after:h-px after:bg-white/10">
         <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
             <div className="relative flex h-16 items-center justify-between">
-                <div class="absolute inset-y-0 left-0 flex items-center">
+                <div className="absolute inset-y-0 left-0 flex items-center">
                     <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
                         <span className=''>Logo</span>
                     </div>
