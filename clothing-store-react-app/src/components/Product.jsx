8ef0e045029c1e3c843a673cca3314cf1e645b5e
Jauniew21 <jauniew21@gmail.com>
2025-11-23 15:36:46 -0700

    Single Product View skeleton

--- clothing-store-react-app/src/components/Product.jsx
+++ clothing-store-react-app/src/components/Product.jsx
@@ -4,11 +4,26 @@ import placeholder from '../assets/shop-placeholder.png'
 const Product = (props) => {
     const { id } = useParams();
     const thisProduct = props.products.find(prod => prod.id == id)
-    console.log(thisProduct)
 
     return(<div>
         <img src={placeholder} alt="placeholder image" className='size-72'/>
+        <img src={placeholder} alt="placeholder image" className='size-36'/>
+        <img src={placeholder} alt="placeholder image" className='size-36'/>
         <p>{thisProduct.name}</p>
+        <p>Price: ${thisProduct.price.toFixed(2)}</p>
+        <p>{thisProduct.description}</p>
+        <label for="quantity">Quantity:</label>
+        <input type="number" name="quantity" min="1"/>
+        {thisProduct.sizes.map(s => <div>
+            <button>{s}</button>
+        </div>)}
+        {/* {thisProduct.color.map(c => <div>
+            <button className={`background-color: ${c.hex}`}>{c.name}</button>
+        </div>)} */}
+        <button>+ Add to Cart</button>
+
+        <p>Similar Products: </p>
+        {/* Similar Products section */}
     </div>)
 }
 
