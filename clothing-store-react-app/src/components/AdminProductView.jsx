dfa3af542b6a24e163e1da8fc88c27e37c8b4347
jauniew21 <jauniew21@gmail.com>
2025-12-06 20:39:07 -0700

    fixed bug?

--- clothing-store-react-app/src/components/AdminProductView.jsx
+++ clothing-store-react-app/src/components/AdminProductView.jsx
@@ -14,90 +14,90 @@ const AdminProductView = (props) => {
             <Drawer open={open} onClose={toggleDrawer(false)}
             className=''>
                 <p>Admin Info</p>
-                <p>{props.product.name} Sales Information</p>
+                <p>{props.product?.name} Sales Information</p>
                 
                 <label for="dom_sales">Domestic Sales:</label>
                 <input
                     name='dom_sales'
                     type="text"
-                    value={props.product.sales.domestic.toFixed(2)}
+                    value={props.product?.sales.domestic.toFixed(2)}
                 />
 
                 <label for="int_sales">International Sales:</label>
                 <input
                     name='int_sales'
                     type="text"
-                    value={props.product.sales.international.toFixed(2)}
+                    value={props.product?.sales.international.toFixed(2)}
                 />
 
                 <label for="tot_sales">Total Sales:</label>
                 <input
                     name='tot_sales'
                     type="text"
-                    value={props.product.sales.total.toFixed(2)}
+                    value={props.product?.sales.total.toFixed(2)}
                 />
 
                 <label for="dom_gross">Domestic Gross:</label>
                 <input
                     name='dom_gross'
                     type="text"
-                    value={(props.product.sales.domestic * props.product.price).toFixed(2)}
+                    value={(props.product?.sales.domestic * props.product?.price).toFixed(2)}
                 />
 
                 <label for="int_gross">International Gross:</label>
                 <input
                     name='int_gross'
                     type="text"
-                    value={(props.product.sales.international * props.product.price).toFixed(2)}
+                    value={(props.product?.sales.international * props.product?.price).toFixed(2)}
                 />
 
                 <label for="total_gross">Total Gross:</label>
                 <input
                     name='total_gross'
                     type="text"
-                    value={(props.product.sales.total * props.product.price).toFixed(2)}
+                    value={(props.product?.sales.total * props.product?.price).toFixed(2)}
                 />
 
                 <label for="dom_cost">Domestic Cost:</label>
                 <input
                     name='dom_cost'
                     type="text"
-                    value={(props.product.sales.domestic * props.product.cost).toFixed(2)}
+                    value={(props.product?.sales.domestic * props.product?.cost).toFixed(2)}
                 />
 
                 <label for="int_cost">International Cost:</label>
                 <input
                     name='int_cost'
                     type="text"
-                    value={(props.product.sales.international * props.product.cost).toFixed(2)}
+                    value={(props.product?.sales.international * props.product?.cost).toFixed(2)}
                 />
 
                 <label for="total_cost">Total Cost:</label>
                 <input
                     name='total_cost'
                     type="text"
-                    value={(props.product.sales.total * props.product.cost).toFixed(2)}
+                    value={(props.product?.sales.total * props.product?.cost).toFixed(2)}
                 />
 
                 <label for="dom_profit">Domestic Profit:</label>
                 <input
                     name='dom_profit'
                     type="text"
-                    value={(props.product.sales.domestic - props.product.cost).toFixed(2)}
+                    value={(props.product?.sales.domestic - props.product?.cost).toFixed(2)}
                 />
 
                 <label for="int_profit">International Profit:</label>
                 <input
                     name='int_profit'
                     type="text"
-                    value={(props.product.sales.international - props.product.cost).toFixed(2)}
+                    value={(props.product?.sales.international - props.product?.cost).toFixed(2)}
                 />
 
                 <label for="total_profit">Total Profit:</label>
                 <input
                     name='total_profit'
                     type="text"
-                    value={(props.product.sales.total - props.product.cost).toFixed(2)}
+                    value={(props.product?.sales.total - props.product?.cost).toFixed(2)}
                 />
             </Drawer>
         </div>
