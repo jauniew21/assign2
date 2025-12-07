89eac668598b0ff8309b9b68b9761135abe521eb
Michelle Hao <mhao306@mtroyal.ca>
2025-12-06 23:28:03 -0700

    added 2 tables

--- clothing-store-react-app/src/components/DashboardView.jsx
+++ clothing-store-react-app/src/components/DashboardView.jsx
@@ -1,8 +1,67 @@
 
 
 const DashboardView = (props) => {
-    return(<div>
+    const topSell = [...props.products].sort((a, b) => { return b.sales.total - a.sales.total })
+    const topProfit = [...props.products].sort((a, b) => { return (b.sales.total * b.price) - (a.sales.total * a.price)})
+
+    return (<div>
+        <p>Sales Dashboard</p>
         <p>poopyhead</p>
+        <div>
+            <p>Top 10 Selling Products</p>
+            <table>
+                <tr>
+                    <th>Product Name</th>
+                    <th>Gender</th>
+                    <th>Category</th>
+                    <th>Total Sales (#)</th>
+                </tr>
+                {topSell.slice(0,10).map(p => {
+                    return (<tr>
+                        <td>{p.name}</td>
+                        <td>{p.gender}</td>
+                        <td>{p.category}</td>
+                        <td>{p.sales.total}</td>
+                    </tr>)
+                })}
+            </table>
+        </div>
+        <div>
+            <p>Top 10 Profitable Products</p>
+            <table>
+                <tr>
+                    <th>Product Name</th>
+                    <th>Gender</th>
+                    <th>Category</th>
+                    <th>Total Sales (#)</th>
+                    <th>Profit ($)</th>
+                </tr>
+                {topProfit.slice(0,10).map(p => {
+                    return (<tr>
+                        <td>{p.name}</td>
+                        <td>{p.gender}</td>
+                        <td>{p.category}</td>
+                        <td>{p.sales.total}</td>
+                        <td>${(p.sales.total * p.price).toFixed(2)}</td>
+                    </tr>)
+                })}
+            </table>
+        </div>
+        <div>
+            <p>Sales + Profit by Category</p>
+            <p>table, category name, summed sales, profit value, sorted by category</p>
+            <p>Category</p>
+            <p>Sales $</p>
+            <p>Profit $</p>
+        </div>
+        <div>
+            <p>Pie Chart</p>
+            <p>sales number by gender</p>
+        </div>
+        <div>
+            <p>Pie Chart</p>
+            <p>sales numbers by category</p>
+        </div>
     </div>)
 }
 
