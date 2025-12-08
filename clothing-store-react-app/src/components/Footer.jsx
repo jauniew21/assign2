6577ee48b6bc00ba4868107d32fc9b3cf45b1862
jauniew21 <jauniew21@gmail.com>
2025-12-08 02:38:13 -0700

    footer done?

--- clothing-store-react-app/src/components/Footer.jsx
+++ clothing-store-react-app/src/components/Footer.jsx
@@ -1,9 +1,20 @@
 
 
 const Footer = (props) => {
-    return (<footer className="">
+    return (<footer className="pt-10">
         <div className="flex ">
-            <p>Github Link: https://github.com/jauniew21/assign2</p>
+            <div>
+                We are Jaunie and Michelle and this our second assignment in COMP 4513 - Advanced Web Development. We've made a basic store page using React and Tailwind. 
+                There are several views, both immediately visible (see header) and hidden (admin only). The user is assumed an Admin for practicality reasons. <br />
+                <br />
+                If you log yourself out, email: 123@gmail.com, and password: 123. Yes, I did do a Cryptography class, thanks for asking.
+                <br /> <br />
+                
+                MaterialUI was used to create all modals (including this one) and charts. And DaisyUI was used to create the carousel on the Home Page. <br />
+                <br />
+
+                Github Repo: https://github.com/jauniew21/assign2/tree/main
+            </div>
         </div>
     </footer>)
 }
