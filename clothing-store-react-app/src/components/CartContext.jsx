7820669086759023bbc5193f7eecda94d46a9e8a
Michelle Hao <mhao306@mtroyal.ca>
2025-12-08 19:34:02 -0700

    whoops

--- clothing-store-react-app/src/components/CartContext.jsx
+++ clothing-store-react-app/src/components/CartContext.jsx
@@ -12,29 +12,6 @@ const CartContextProvider = (props) => {
         localStorage.setItem("cart", JSON.stringify(cart))
     }, [cart])
 
-    const addItem = (prod) => {
-        setCart(current => {
-            const existing = cart.find(item => item.id === prod.id && item.size === selSize)
-            if (!existing) {
-                return [...current, {...prod, quantity: 1}]
-        }
-        return current.map(i => )
-
-        c
-
-        
-            const newCart = cart.map(item =>
-                item.id === prod.id && item.size === selSize
-                    ? { ...item, quantity: item.quantity + qty }
-                    : item
-            )
-            setCart(newCart)
-        } else {
-            setCart([...cart, { ...prod, quantity: qty, size: selSize }])
-        }
-        alert(`Added ${qty} x ${prod.name} ${selSize} to cart`)
-    }
-
     return (
         <CartContext.Provider value={{ cart, setCart }}>
             {props.children}
